fa03f783e77db4491ca2e78753cb7a122925b3d5
James Reeves <jreeves@weavejester.com>
2016-06-19 17:31:09 +0100

    Add placeholder for scene builder

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -1,5 +1,7 @@
 (ns example.core
   (:require [impi.core :as impi]))
 
-(let [renderer (impi/renderer [400 300])]
-  (impi/mount renderer (.getElementById js/document "app")))
+(let [renderer (impi/renderer [400 300])
+      element  (.getElementById js/document "app")]
+  (impi/mount renderer element)
+  (impi/render renderer {}))
--- src/impi/core.cljs
+++ src/impi/core.cljs
@@ -6,3 +6,17 @@
 
 (defn mount [renderer element]
   (.appendChild element (.-view renderer)))
+
+(defn build [scene]
+  (let [stage   (js/PIXI.Container.)
+        texture (js/PIXI.Texture.fromImage "img/bunny.png")
+        sprite  (js/PIXI.Sprite. texture)]
+    (set! (-> sprite .-anchor .-x) 0.5)
+    (set! (-> sprite .-anchor .-y) 0.5)
+    (set! (-> sprite .-position .-x) 200)
+    (set! (-> sprite .-position .-y) 150)
+    (.addChild stage sprite)
+    stage))
+
+(defn render [renderer scene]
+  (.render renderer (build scene)))

c98f01e96c1a7986e672fba8d723b5943f95c23d
James Reeves <jreeves@weavejester.com>
2016-06-19 14:32:37 +0100

    Add Pixi.js 4.0.0 pre-release dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [weavejester/pixi "4.0.0-5c77800-0"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:builds

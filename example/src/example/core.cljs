1966b0f580dd0fdfd1801e0283425086b3d6c24e
James Reeves <jreeves@weavejester.com>
2016-06-19 18:51:10 +0100

    Ensure example renderer is defined only once

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -1,7 +1,9 @@
 (ns example.core
   (:require [impi.core :as impi]))
 
-(let [renderer (impi/renderer [400 300])
-      element  (.getElementById js/document "app")]
+(defonce renderer
+  (impi/renderer [400 300]))
+
+(let [element (.getElementById js/document "app")]
   (impi/mount renderer element)
   (impi/render renderer {}))

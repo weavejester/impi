d9d52a93ad31f4b155cb40f656455a67301e9739
James Reeves <jreeves@weavejester.com>
2016-06-19 13:51:11 +0100

    Add example project

---
+++ example/project.clj
@@ -0,0 +1,9 @@
+(defproject impi/example "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [impi "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.3"]])

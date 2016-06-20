86afa112c8b4ec3c13c4c73d2802f71889dc78d1
James Reeves <jreeves@weavejester.com>
2016-06-20 11:38:05 +0100

    Get sprite textures from loader

--- src/impi/core.cljs
+++ src/impi/core.cljs
@@ -1,4 +1,5 @@
 (ns impi.core
+  (:refer-clojure :exclude [load])
   (:require cljsjs.pixi))
 
 (defn renderer [[w h]]
@@ -8,10 +9,19 @@
   (set! (.-innerHTML element) "")
   (.appendChild element (.-view renderer)))
 
+(def ^:private loader
+  (js/PIXI.loaders.Loader.))
+
+(defn load [assets callback]
+  (run! #(.add loader %) assets)
+  (.load loader callback))
+
+(defn get-texture [asset]
+  (.-texture (aget (.-resources loader) asset)))
+
 (defn build [scene]
   (let [stage   (js/PIXI.Container.)
-        texture (js/PIXI.Texture.fromImage "img/bunny.png")
-        sprite  (js/PIXI.Sprite. texture)]
+        sprite  (js/PIXI.Sprite. (get-texture "img/bunny.png"))]
     (set! (-> sprite .-anchor .-x) 0.5)
     (set! (-> sprite .-anchor .-y) 0.5)
     (set! (-> sprite .-position .-x) 200)
@@ -19,13 +29,6 @@
     (.addChild stage sprite)
     stage))
 
-(def ^:private loader
-  (js/PIXI.loaders.Loader.))
-
-(defn preload [assets callback]
-  (run! #(.add loader %) assets)
-  (.load loader callback))
-
 (defn render [renderer scene]
   (let [render #(.render renderer (build scene))]
-    (preload ["img/bunny.png"] #(js/requestAnimationFrame render))))
+    (load ["img/bunny.png"] #(js/requestAnimationFrame render))))

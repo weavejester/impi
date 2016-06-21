8e64c84dde4a1e5f99a0eaf6ed1bcfef4a23f105
James Reeves <jreeves@weavejester.com>
2016-06-21 14:53:00 +0100

    Add rotation animation to example

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -4,12 +4,19 @@
 (defonce renderer
   (impi/renderer [400 300]))
 
-(def data
-  {:pixi/type            :pixi.type/sprite
-   :pixi.object/position [200 150]
-   :pixi.sprite/anchor   [0.5 0.5]
-   :pixi.sprite/texture  "img/bunny.png"})
+(def state
+  (atom {:pixi/type            :pixi.type/sprite
+         :pixi.object/position [200 150]
+         :pixi.object/rotation 0.0
+         :pixi.sprite/anchor   [0.5 0.5]
+         :pixi.sprite/texture  "img/bunny.png"}))
+
+(defn animate [state]
+  (swap! state update :pixi.object/rotation + 0.1)
+  (js/setTimeout #(animate state) 16))
 
 (let [element (.getElementById js/document "app")]
   (impi/mount renderer element)
-  (impi/render renderer data))
+  (impi/render renderer @state)
+  (add-watch state ::render (fn [_ _ _ s] (impi/render renderer s)))
+  (animate state))

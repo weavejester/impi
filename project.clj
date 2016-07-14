2282c39d92106bd7069a20719de866c6a608b396
James Reeves <jreeves@weavejester.com>
2016-07-14 18:58:12 +0100

    Update Pixi dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
-                 [weavejester/pixi "4.0.0-5c77800-0"]]
+                 [weavejester/pixi "4.0.0-8fdb17a-0"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:builds

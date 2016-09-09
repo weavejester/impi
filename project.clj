a6e2bfafd453cab156bc959bfdd7c63f626149bd
James Reeves <jreeves@weavejester.com>
2016-09-09 18:31:33 +0100

    Release 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject impi "0.0.5"
+(defproject impi "0.0.6"
   :description "ClojureScript library for using Pixi.js through immutable data"
   :url "https://github.com/weavejester/impi"
   :license {:name "Eclipse Public License"
